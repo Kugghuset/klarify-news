4d19050b7879f89667e713e3edac86c643824deb
Hugi <hugiasgeirsson@gmail.com>
2016-10-16 00:19:10 +0000

    add IP log

--- server/api/news/news.controller.js
+++ server/api/news/news.controller.js
@@ -2,11 +2,19 @@
 
 import news from './news.db';
 import utils from '../../utils/utils';
+import winston from 'winston';
+
+winston.add(winston.transports.File, { filename: 'logfile.log' });
 
 /**
  * Route: GET '/api/factProject/all'
  */
 export const getAll = (req, res) => {
+    console.log(res.connection.remoteAddress);
+    console.log(res.headers['x-forwarded-for']);
+    winston.info('New IP!');
+    winston.log('info', res.connection.remoteAddress);
+    winston.log('info', res.headers['x-forwarded-for']);
     news.getAll()
     .then((newsItem) => res.status(200).json(newsItem))
     .catch((err) => utils.handleError(res, err));
